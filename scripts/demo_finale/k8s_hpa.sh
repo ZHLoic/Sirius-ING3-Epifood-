451a60636244fe7aae2800c64c1485ef4e8b7246
Adrien Dos Santos <adriend671@gmail.com>
2026-05-22 06:14:53 +0200

    port mqtt

--- scripts/demo_finale/k8s_hpa.sh
+++ scripts/demo_finale/k8s_hpa.sh
@@ -6,11 +6,12 @@ if [ "$#" -ne 1 ]; then
 fi
 
 IP_MQTT="172.31.250.254"
-RANGE=${500}
+RANGE=500
 
 for i in $(seq 1 $1)
 do
         CASINO=$(( RANDOM % RANGE + 1 ))
+        echo CASINO:$CASINO
         mosquitto_pub -h $IP_MQTT -t lock/telemetry -m '{"badgeId":'$CASINO'}'
         echo "requete à:$IP_MQTT ID:$CASINO fait à $(date +"%H:%M:%S")" >> mock.log
 done
