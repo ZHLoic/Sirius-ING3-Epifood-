bdf2b0665ddff81d6caecd5f606f8a229d2c11e1
Adrien Dos Santos <adriend671@gmail.com>
2026-05-22 06:01:39 +0200

    test mqtt cluster

---
+++ scripts/demo_finale/k8s_hpa.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <nombre_d_essai>"
+    exit 1
+fi
+
+IP_MQTT="172.31.250.254"
+RANGE=${500}
+
+for i in $(seq 1 $1)
+do
+        CASINO=$(( RANDOM % RANGE + 1 ))
+        mosquitto_pub -h $IP_MQTT -t lock/telemetry -m '{"badgeId":'$CASINO'}'
+        echo "requete à:$IP_MQTT ID:$CASINO fait à $(date +"%H:%M:%S")" >> mock.log
+done
